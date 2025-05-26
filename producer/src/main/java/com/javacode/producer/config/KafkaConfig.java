60cbf34a66cc667fd40783fc558d8b2863edb3fb
Alina <alinavik93@gmail.com>
2025-05-26 16:41:47 +0500

    Практическое задание - kafka - партиции

--- producer/src/main/java/com/javacode/producer/config/KafkaConfig.java
+++ producer/src/main/java/com/javacode/producer/config/KafkaConfig.java
@@ -1,10 +1,12 @@
 package com.javacode.producer.config;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.kafka.core.ProducerFactory;
 
 import java.util.HashMap;
@@ -29,4 +31,10 @@ public class KafkaConfig {
                 .build();
     }
 
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        return new KafkaAdmin(configs);
+    }
 }
